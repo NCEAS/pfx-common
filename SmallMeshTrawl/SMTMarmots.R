c91cefd923054c3c93e920017c23f1b54d8c52a0
Colette Ward <colette.l.ward@gmail.com>
2015-10-19 14:36:22 -0700

    added annual CUEs to reflect catch organized by rows not columns

--- SmallMeshTrawl/SMTMarmots.R
+++ SmallMeshTrawl/SMTMarmots.R
@@ -20,29 +20,125 @@ Marmots = SMT2 %>%
 
 
 
-# Which taxa comprise the most biomass?
-MarImp = as.data.frame(colSums(Marmots[,18:335])) # sum each taxa's CUE across all years
-#View(MarImp)
-colnames(MarImp)[which(colnames(MarImp)=="colSums(Marmots[, 18:335])")] = "TotCUE" #rename the column
-MarImp[["colSums(SMT4[,18:335])"]]
-MarImp1 = data.frame(Taxon=colnames(Marmots[,18:335]),MarImp) %>%
-  mutate(percent=(TotCUE/sum(TotCUE))*100) %>%
-  arrange(desc(percent))
-View(MarImp1)
+
+#Groupings:
+# Forage fish Group (Eulachon, Capelin, Sandlance, Sandfish, Herring, Tomcod)
+# Shrimp (Pink vs all others)
+# Tanner crab
+# (Adult) pollock
+# Arrowtooth flounder
+# Demersal predators Group (Flounder, Sole, Halibut, Gadids)
+# Dogfish (and Dogfish + Sleeper)
+# Jellies
 
 
-# Annual means:
-Marmots1 = aggregate(Marmots[18:335],
-                     by = list(year = Marmots$year),
-                     FUN = "mean")
-View(Marmots1)
-dim(Marmots1) # 28 319
+# see unique taxa info
+uniTaxa=SMTbiol %>%
+  filter(!duplicated(raceCode)) %>%
+  select(-cruise, -haul, -catchKg, -catchNum)
+View(uniTaxa)
 
 
+# Construct annual CUE. 
+# Use sum instead of mean because incidences of zero catch are not present when sp catch is organized by rows instead of columns. 
 
-# StErr for annual means:
-sterr = function(x) sd(x)/sqrt(length(x))
-Marmots1Err = aggregate(Marmots[18:335], 
-                        by = list(year = Marmots$year),
-                        FUN = "sterr")
-View(Marmots1Err)
+# Pink Shrimp
+PinkShrimp = Marmots %>%
+  filter(sciName == "Pandalus borealis") %>%
+  group_by(year) %>%
+  summarise(PinkShrimp=sum(cue)) %>%
+  ungroup
+View(PinkShrimp)
+
+
+# Shrimp excluding Pink shrimp
+ShrimpNoPink = Marmots %>%
+  filter(infraorder == "Caridea") %>%
+  filter(sciName != "Pandalus borealis") %>% #exclude Pink shrimp
+  group_by(year) %>%
+  summarise(ShrimpNoPink=sum(cue)) %>%
+  ungroup
+View(ShrimpNoPink)
+
+
+# Tanner crab
+Tanner = Marmots %>%
+  filter(sciName == "Chionoecetes bairdi") %>%
+  group_by(year) %>%
+  summarise(Tanner=sum(cue)) %>%
+  ungroup
+View(Tanner)
+
+
+# Adult pollock
+PollockAd = Marmots %>%
+  filter(raceCode == 21740) %>%
+  group_by(year) %>%
+  summarise(PollockAd=sum(cue)) %>%
+  ungroup
+View(PollockAd)
+
+
+# Juvenile pollock
+PollockJuv = Marmots %>%
+  filter(raceCode == 21741) %>%
+  group_by(year) %>%
+  summarise(PollockJuv=sum(cue)) %>%
+  ungroup
+View(PollockJuv)
+
+
+# Arrowtooth Flounder
+Arrowtooth = Marmots %>%
+  filter(sciName == "Reinhardtius stomias") %>%
+  group_by(year) %>%
+  summarise(Arrowtooth=sum(cue)) %>%
+  ungroup
+View(Arrowtooth)
+
+
+# Demersal predators (Flounder, Sole, Halibut, Gadids, excluding Pollock)
+DemPred = Marmots %>%
+  filter(order %in% c("Pleuronectiformes", "Gadiformes")) %>%
+  filter(sciName != "Gadus chalcogrammus") %>% # exclude Pollock
+  group_by(year) %>%
+  summarise(DemPred=sum(cue)) %>%
+  ungroup
+View(DemPred)
+
+
+# Dogfish
+Dogfish = Marmots %>%
+  filter(sciName == "Squalus acanthias") %>%
+  group_by(year) %>%
+  summarise(Dogfish=sum(cue)) %>%
+  ungroup
+View(Dogfish)
+
+
+# Jellies
+Jellies = Marmots %>%
+  filter(subphylum == "Medusozoa" | sciName %in% c("Ctenophora", "Beroe")) %>%
+  group_by(year) %>%
+  summarise(Jellies=sum(cue)) %>%
+  ungroup
+View(Jellies)
+
+
+# create dataframe with years
+MarmotsAnnCue=data.frame('year'=c(1972:2013))
+
+# Merge in the taxon-specific biomass data
+MarmotsAnnCue <- merge(MarmotsAnnCue,ForageFish,all.x=T)
+MarmotsAnnCue <- merge(MarmotsAnnCue,PinkShrimp,all.x=T)
+MarmotsAnnCue <- merge(MarmotsAnnCue,ShrimpNoPink,all.x=T)
+MarmotsAnnCue <- merge(MarmotsAnnCue,Tanner,all.x=T)
+MarmotsAnnCue <- merge(MarmotsAnnCue,PollockAd,all.x=T)
+MarmotsAnnCue <- merge(MarmotsAnnCue,PollockJuv,all.x=T)
+MarmotsAnnCue <- merge(MarmotsAnnCue,Arrowtooth,all.x=T)
+MarmotsAnnCue <- merge(MarmotsAnnCue,DemPred,all.x=T)
+MarmotsAnnCue <- merge(MarmotsAnnCue,Dogfish,all.x=T)
+MarmotsAnnCue <- merge(MarmotsAnnCue,Jellies,all.x=T)
+
+View(MarmotsAnnCue)
+
