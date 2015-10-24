14d9c88eac63a5b0d29c136193176eee859bb655
Colette Ward <colette.l.ward@gmail.com>
2015-10-23 19:54:41 -0700

    recalculated Annual CUEs as mean CUE of non-zero catches

--- SmallMeshTrawl/SMTMarmots.R
+++ SmallMeshTrawl/SMTMarmots.R
@@ -4,21 +4,11 @@
 #####                       October 2015                         #####
 ######################################################################
 
-# call output from Small Mesh Trawl cleaning script
-sourceDir <- function(path, trace=TRUE) {
-  for (nm in list.files(path, pattern = "[.][Rr]$")) {
-    if(trace) cat(nm,":")
-    source(file.path(path, nm))
-    if(trace) cat("\n")
-  }
-}
-sourceDir("SmallMeshTrawl")
+# first run SmallMeshTrawlProcessingScript.R
 #View(SMT2)
 
-Marmots = SMT2 %>%
+Marmots <- SMT2 %>%
   filter(bay %in% c(1002, 1003))
-
-
 
 
 #Groupings:
@@ -27,106 +17,125 @@ Marmots = SMT2 %>%
 # Tanner crab
 # (Adult) pollock
 # Arrowtooth flounder
-# Demersal predators Group (Flounder, Sole, Halibut, Gadids)
+# Demersal predators Group (Flounder, Sole, Halibut, Gadids; excluding Pollock & Arrowtooth)
 # Dogfish (and Dogfish + Sleeper)
 # Jellies
 
 
 # see unique taxa info
-uniTaxa=SMTbiol %>%
-  filter(!duplicated(raceCode)) %>%
-  select(-cruise, -haul, -catchKg, -catchNum)
-View(uniTaxa)
+#uniTaxa <- ASMTbiol %>%
+#  filter(!duplicated(raceCode)) %>%
+#  select(-cruise, -haul, -catchKg, -catchNum)
+#View(uniTaxa)
 
 
-# Construct annual CUE. 
-# Use sum instead of mean because incidences of zero catch are not present when sp catch is organized by rows instead of columns. 
+# Construct annual CUE (means of non-zero catches)
+# Forage Fish (Eulachon, Capelin, Sandlance, Sandfish, Herring, Tomcod) add smelts? (family == "Osmeridae")
+ForageFish <- Marmots %>%
+  filter(sciName %in% c("Thaleichthys pacificus", "Mallotus villosus", "Ammodytes hexapterus", 
+                        "Trichodon trichodon", "Clupea pallasii", "Microgadus proximus")) %>%
+  group_by(year, Sample) %>% # group by year & haul
+  summarise(FFByHaul=sum(cue)) %>% # sum cue for all group members collected in each haul
+  ungroup %>%
+  group_by(year) %>% # group by year
+  summarise(ForageFish=mean(FFByHaul, na.rm=T)) %>% # calculate mean annual CUEs across hauls
+  ungroup
+#View(ForageFish)
+
 
 # Pink Shrimp
-PinkShrimp = Marmots %>%
+PinkShrimp <- Marmots %>%
   filter(sciName == "Pandalus borealis") %>%
   group_by(year) %>%
-  summarise(PinkShrimp=sum(cue)) %>%
+  summarise(PinkShrimp=mean(cue)) %>%
   ungroup
-View(PinkShrimp)
+#View(PinkShrimp)
 
 
 # Shrimp excluding Pink shrimp
-ShrimpNoPink = Marmots %>%
+ShrimpNoPink <- Marmots %>%
   filter(infraorder == "Caridea") %>%
   filter(sciName != "Pandalus borealis") %>% #exclude Pink shrimp
+  group_by(year, Sample) %>%
+  summarise(ShrByHaul=sum(cue)) %>% 
+  ungroup %>%
   group_by(year) %>%
-  summarise(ShrimpNoPink=sum(cue)) %>%
+  summarise(ShrimpNoPink=mean(ShrByHaul, na.rm=T)) %>%
   ungroup
-View(ShrimpNoPink)
+#View(ShrimpNoPink)
 
 
 # Tanner crab
-Tanner = Marmots %>%
+Tanner <- Marmots %>%
   filter(sciName == "Chionoecetes bairdi") %>%
   group_by(year) %>%
-  summarise(Tanner=sum(cue)) %>%
+  summarise(Tanner=mean(cue)) %>%
   ungroup
-View(Tanner)
+#View(Tanner)
 
 
 # Adult pollock
-PollockAd = Marmots %>%
+PollockAd <- Marmots %>%
   filter(raceCode == 21740) %>%
   group_by(year) %>%
-  summarise(PollockAd=sum(cue)) %>%
+  summarise(PollockAd=mean(cue)) %>%
   ungroup
-View(PollockAd)
+#View(PollockAd)
 
 
 # Juvenile pollock
-PollockJuv = Marmots %>%
+PollockJuv <- Marmots %>%
   filter(raceCode == 21741) %>%
   group_by(year) %>%
-  summarise(PollockJuv=sum(cue)) %>%
+  summarise(PollockJuv=mean(cue)) %>%
   ungroup
-View(PollockJuv)
+#View(PollockJuv)
 
 
 # Arrowtooth Flounder
-Arrowtooth = Marmots %>%
+Arrowtooth <- Marmots %>%
   filter(sciName == "Reinhardtius stomias") %>%
   group_by(year) %>%
-  summarise(Arrowtooth=sum(cue)) %>%
+  summarise(Arrowtooth=mean(cue)) %>%
   ungroup
-View(Arrowtooth)
+#View(Arrowtooth)
 
 
-# Demersal predators (Flounder, Sole, Halibut, Gadids, excluding Pollock)
-DemPred = Marmots %>%
+# Demersal predators (Flounder, Sole, Halibut, Gadids, excluding Pollock, Arrowtooth, Tomcod)
+DemPred <- Marmots %>%
   filter(order %in% c("Pleuronectiformes", "Gadiformes")) %>%
   filter(sciName != "Gadus chalcogrammus") %>% # exclude Pollock
-  group_by(year) %>%
-  summarise(DemPred=sum(cue)) %>%
+  filter(sciName != "Reinhardtius stomias") %>% # exclude Arrowtooth
+  filter(sciName != "Microgadus proximus") %>% # exclude Tomcod
+  group_by(year, Sample) %>%
+  summarise(DPByHaul=sum(cue)) %>%
+  ungroup %>%
+  group_by(year) %>% # group by year
+  summarise(DemPred=mean(DPByHaul, na.rm=T)) %>% # calculate mean annual CUEs across hauls
   ungroup
-View(DemPred)
+#View(DemPred)
 
 
 # Dogfish
-Dogfish = Marmots %>%
+Dogfish <- Marmots %>%
   filter(sciName == "Squalus acanthias") %>%
   group_by(year) %>%
-  summarise(Dogfish=sum(cue)) %>%
+  summarise(Dogfish=mean(cue)) %>%
   ungroup
-View(Dogfish)
+#View(Dogfish)
 
 
 # Jellies
-Jellies = Marmots %>%
+Jellies <- Marmots %>%
   filter(subphylum == "Medusozoa" | sciName %in% c("Ctenophora", "Beroe")) %>%
   group_by(year) %>%
   summarise(Jellies=sum(cue)) %>%
   ungroup
-View(Jellies)
+#View(Jellies)
 
 
 # create dataframe with years
-MarmotsAnnCue=data.frame('year'=c(1972:2013))
+MarmotsAnnCue <- data.frame('year'=c(1976:2013))
 
 # Merge in the taxon-specific biomass data
 MarmotsAnnCue <- merge(MarmotsAnnCue,ForageFish,all.x=T)
